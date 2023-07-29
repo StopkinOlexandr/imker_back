9155ebd1ed08a027026b432471468992daada686
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-07-29 04:48:06 +0200

    add DELETE method, add InitilizationData method

--- src/main/java/de/imker/controllers/api/RequestsApi.java
+++ src/main/java/de/imker/controllers/api/RequestsApi.java
@@ -5,6 +5,10 @@ import de.imker.dto.RequestDto;
 import de.imker.dto.RequestsDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.http.HttpStatus;
@@ -17,17 +21,33 @@ import org.springframework.web.bind.annotation.*;
 public interface RequestsApi {
 
 
-  @Operation(summary = "User Request Creation", description = "Accessible to all users")
+  @Operation(summary = "Create user request", description = "Accessible to all users")
   @PostMapping
 //  @CrossOrigin(origins = "http://localhost:5173")
   @ResponseStatus(HttpStatus.CREATED)
   RequestDto addRequest(@Parameter(required = true, description = "Request")
-                               @RequestBody NewRequestDto newRequest);
+                        @RequestBody NewRequestDto newRequest);
 
-  @Operation(summary = "User Request List", description = "Accessible to all users")
+  @Operation(summary = "Get all requests list", description = "Accessible to all users")
   @GetMapping
 //  @CrossOrigin(origins = "http://localhost:5173")
   @ResponseStatus(HttpStatus.OK)
   RequestsDto getAllRequests();
+
+  @Operation(summary = "Delete request", description = "Accessible to all users")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "404", description = "Request not found",
+          content = {
+              @Content()
+          }),
+      @ApiResponse(responseCode = "200", description = "Deleted request",
+          content = {
+              @Content(mediaType = "application/json", schema = @Schema(implementation = RequestDto.class))
+          })
+  })
+  @CrossOrigin(origins = "http://localhost:5173")
+  @DeleteMapping("/{request-id}")
+  RequestDto deleteRequest(@Parameter(required = true, description = "Request id", example = "2")
+                           @PathVariable("request-id") Long idRequest);
 
 }
