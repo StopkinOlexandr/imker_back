abfd8c342e70d556acf3a1d97d798f72f72c46c8
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-16 16:18:05 +0200

    add getFiles endpoint

--- src/main/java/de/imker/controllers/FilesController.java
+++ src/main/java/de/imker/controllers/FilesController.java
@@ -2,14 +2,14 @@ package de.imker.controllers;
 
 import de.imker.dto.FileUploadDto;
 import de.imker.services.impl.FilesServiceImpl;
-import org.jetbrains.annotations.NotNull;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -25,8 +25,23 @@ public class FilesController {
   private FilesServiceImpl filesService;
 
   @PostMapping(path = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public FileUploadDto uploadFile(@NotNull @RequestParam("file") MultipartFile file) throws IOException {
+  public FileUploadDto uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
 
     return filesService.uploadFile(file);
+  }
+
+  @GetMapping(path = "/{file-id}", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<Resource> getFile(@Parameter(required = true, description = "File ID", example = "1")
+                                          @PathVariable("file-id") Long fileId) {
+
+    Resource resource = filesService.getFileResource(fileId);
+
+    if (resource != null) {
+      return ResponseEntity.ok()
+          .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+          .body(resource);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
   }
 }
