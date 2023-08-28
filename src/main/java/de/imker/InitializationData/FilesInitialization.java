2c16b7ce368e16baf6947d2260d2792105643d3e
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-08-28 02:38:46 +0200

    refactor files

--- src/main/java/de/imker/InitializationData/FilesInitialization.java
+++ src/main/java/de/imker/InitializationData/FilesInitialization.java
@@ -39,7 +39,7 @@ public class FilesInitialization {
           .creationTime(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
           .category(FileUpload.Category.POST)
           .originalName("kopfbild_vereine_03[1].jpg")
-          .storedName("!1-7f1bb7eb-231b-4535-b917-ca7819841540kopfbild_vereine_03[1].jpg")
+          .storedName("!001-7f1bb7eb-231b-4535-b917-ca7819841540kopfbild_vereine_03[1].jpg")
           .fileType("image/jpeg")
           .size((long) 33632)
           .build();
@@ -50,7 +50,7 @@ public class FilesInitialization {
           .creationTime(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
           .category(FileUpload.Category.POST)
           .originalName("header-ratgeber-bienen[1].jpg")
-          .storedName("!2-b3d40dd7-5813-4ecc-b1fc-188e685061ddheader-ratgeber-bienen[1].jpg")
+          .storedName("!002-b3d40dd7-5813-4ecc-b1fc-188e685061ddheader-ratgeber-bienen[1].jpg")
           .fileType("image/jpeg")
           .size((long) 27381)
           .build();
@@ -61,7 +61,7 @@ public class FilesInitialization {
           .creationTime(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
           .category(FileUpload.Category.POST)
           .originalName("5376145_original2560_1zX9HW_qP3j6P[1].jpg")
-          .storedName("!3-871d3f92-bff9-4333-a0bd-62e63d3d37845376145_original2560_1zX9HW_qP3j6P[1].jpg")
+          .storedName("!003-871d3f92-bff9-4333-a0bd-62e63d3d37845376145_original2560_1zX9HW_qP3j6P[1].jpg")
           .fileType("image/jpeg")
           .size((long) 21111)
           .build();
@@ -72,7 +72,7 @@ public class FilesInitialization {
           .creationTime(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
           .category(FileUpload.Category.POST)
           .originalName("honigbienen-01[1].jpg")
-          .storedName("!4-707c4d7c-6fc7-4f8f-92b8-c287f1520442honigbienen-01[1].jpg")
+          .storedName("!004-707c4d7c-6fc7-4f8f-92b8-c287f1520442honigbienen-01[1].jpg")
           .fileType("image/jpeg")
           .size((long) 75514)
           .build();
@@ -83,7 +83,7 @@ public class FilesInitialization {
           .creationTime(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
           .category(FileUpload.Category.POST)
           .originalName("w940_h528_x470_y264_5736869295a8f6e7[1].jpg")
-          .storedName("!5-6ed43282-1516-4578-ba2c-1327982b4365w940_h528_x470_y264_5736869295a8f6e7[1].jpg")
+          .storedName("!005-6ed43282-1516-4578-ba2c-1327982b4365w940_h528_x470_y264_5736869295a8f6e7[1].jpg")
           .fileType("image/jpeg")
           .size((long) 32690)
           .build();
@@ -94,7 +94,7 @@ public class FilesInitialization {
           .creationTime(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
           .category(FileUpload.Category.POST)
           .originalName("Honigvielfalt[1].jpg")
-          .storedName("!6-01c11f85-b8b9-46df-99c1-51cd7b456099Honigvielfalt[1].jpg")
+          .storedName("!006-01c11f85-b8b9-46df-99c1-51cd7b456099Honigvielfalt[1].jpg")
           .fileType("image/jpeg")
           .size((long) 41939)
           .build();
@@ -105,7 +105,7 @@ public class FilesInitialization {
           .creationTime(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
           .category(FileUpload.Category.POST)
           .originalName("Honigglaeser_980_rdax_720x316_80[1].jpg")
-          .storedName("!7-ec0b4ac4-73de-4251-8a11-ef387c42c8a8Honigglaeser_980_rdax_720x316_80[1].jpg")
+          .storedName("!007-ec0b4ac4-73de-4251-8a11-ef387c42c8a8Honigglaeser_980_rdax_720x316_80[1].jpg")
           .fileType("image/jpeg")
           .size((long) 30980)
           .build();
@@ -116,7 +116,7 @@ public class FilesInitialization {
           .creationTime(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
           .category(FileUpload.Category.POST)
           .originalName("die-bienenfluesterer_401627[1].jpg")
-          .storedName("!8-030b5f71-91d6-4dc8-890c-e27ecd6ab122die-bienenfluesterer_401627[1].jpg")
+          .storedName("!008-030b5f71-91d6-4dc8-890c-e27ecd6ab122die-bienenfluesterer_401627[1].jpg")
           .fileType("image/jpeg")
           .size((long) 38958)
           .build();
