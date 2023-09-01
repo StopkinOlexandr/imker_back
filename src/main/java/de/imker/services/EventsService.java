1005157e95ab5e9ef55949ab2c386db1c879e476
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-09-01 18:10:13 +0200

    clean api, fix some tests

--- src/main/java/de/imker/services/EventsService.java
+++ src/main/java/de/imker/services/EventsService.java
@@ -10,21 +10,18 @@ public interface EventsService {
     EventDto addEvent(NewEventDto newEvent);
 
 
-    EventsDto getAllEvents(Integer page, Integer pageSize, String orderBy, Boolean desc, String filterBy, String filterValue );
+    EventsDto getAllEvents(Integer page, Integer pageSize, String orderBy, Boolean desc, String filterBy, String filterValue);
 
     EventDto deleteEvent(Long eventId);
 
     EventDto updateEvent(Long eventId, UpdateEventDto updateEvent);
 
-    EventsDto getEventsOfUser(Long userId);
 
     EventDto getEventById(Long eventId);
 
 
     EventFollowDto followEventById(Long eventId);
 
-
-//    EventFollowDto deleteFollowedEvent(Long followedId);
 
     EventsDto getAllTimeEvents();
 
